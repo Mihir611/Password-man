02ab8a489eb7496d83e0e0d694b3fd99348627db
Mihir <mihir@iotracx.com>
2022-06-28 16:51:10 +0530

    bug fixes

--- passman-backend/server.js
+++ passman-backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config();
-const app = express();
+const app = express().router;
 const bodyParser = require('body-parser');
 const cors = require('cors')
 let database
