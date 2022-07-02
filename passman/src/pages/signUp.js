655933112e80ab51c67876aa5e54145a04f54965
Mihir <mihir@iotracx.com>
2022-07-02 10:22:57 +0530

    Added the register code

---
+++ passman/src/pages/signUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text, View, SafeAreaView} from 'react-native'
+
+const SignUp = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Form works</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default SignUp
---
+++ passman/src/utils/ColorStyles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native'
+
+const ColorStyles = StyleSheet.create({
+    bodyBackground:{
+        backgroundColor:'#101010'
+    },
+    textColor : {
+        color:'#51087E'
+    }
+})
+
+export default ColorStyles
